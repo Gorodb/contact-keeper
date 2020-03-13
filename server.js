4813a30f0c46b64d1ab8f55587d3ba92867dfb4a
Ramis Vakazov <vakazov@restream.rt.ru>
2020-03-13 13:15:01 +0300

    fixes

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const morgan = require('morgan')
 const errorHandler = require('./middleware/mongooseErrorsHandler')
 const path = require('path')
 
+const http = require('http')
+
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+app.set('port', (process.env.PORT || 5000))
 connectDb()
 app.use(express.json({ extended: false }))
 
@@ -53,15 +56,10 @@ app.use('/api/secret', require('./routes/clearDb'))
 
 app.use(errorHandler)
 
-// Static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
-//
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+app.get('/', (req, res) => {
+    res.render('public/index.html')
+})
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+http.createServer(app).listen(app.get('port'), () => {
+    console.log(`Server started on port: ${app.get('port')}`);
+})
